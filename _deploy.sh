a2ce7d01ad34c83419bc9b0dc8bd0190c2da80ea
w201rdada-portfolio-jab684-wxu4250y <juliab@berkeley.edu>
2017-11-22 18:26:02 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -7,7 +7,7 @@ git config user.name "Julia Buffinton"
 # make sure you're on the master branch
 git checkout master
 
-cd /home/oski \
+cd /home/rstudio \
 && rm -rf docs/ \
 && mv -u -T _book/ docs/
 
